e1bdaab269213714de044a8fc652cc44fabf0c3f
Eugene Scherba <escherba@livefyre.com>
2014-04-16 12:36:59 -0700

    Fix the problem with iframe display

---
+++ CommentStream/Classes/UIWebView+ScrollViewContentSize.h
@@ -0,0 +1,15 @@
+//
+//  UIWebView+ScrollViewContentSize.h
+//  CommentStream
+//
+//  Created by Eugene Scherba on 4/16/14.
+//  Copyright (c) 2014 Livefyre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (ScrollViewContentSize)
+
+-(CGSize)scrollViewContentSizeWithWidth:(CGFloat)width;
+
+@end
